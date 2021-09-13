1f4828789c78e1344b465506d75ed3d5ffbd27e5
Lam Dinh Truong <dinhtruonglam001@gmail.com>
2021-09-13 17:23:54 +0300

    .

---
+++ src/Update_sql.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+public class Update_sql {
+    public static void update() throws SQLException{
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root");
+        Statement st = con.createStatement();
+        String sql = "update employee " +
+                "set ename = 'ha' " +
+                "where age = 20";
+        st.executeUpdate(sql);
+        ResultSet res = st.executeQuery("select * from employee");
+        while(res.next()){
+            System.out.println(res.getString("ename") + " " + res.getString("age"));
+        }
+    }
+}
