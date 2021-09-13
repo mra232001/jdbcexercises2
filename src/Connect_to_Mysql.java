9d8e1b53bc2e8f285f904fdb31c658d57f145c81
Lam Dinh Truong <dinhtruonglam001@gmail.com>
2021-09-13 17:00:25 +0300

    .

--- src/Connect_to_Mysql.java
+++ src/Connect_to_Mysql.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.function.Consumer;
 
 public class Connect_to_Mysql {
-    public static void main(String ...args) throws SQLException {
+    public static void connect() throws SQLException {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root");
         Statement st = con.createStatement();
         ResultSet res = st.executeQuery("select * from employee");
